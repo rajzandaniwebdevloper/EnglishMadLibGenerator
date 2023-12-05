ea5a3d3c40c92154eb63d2f4960f563da61bb713
rajzandaniwebdevloper <142796026+rajzandaniwebdevloper@users.noreply.github.com>
2023-12-05 19:41:34 +0530

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import random
 
 def madlib_generator():
-    # Define lists of nouns, verbs, adjectives, and adverbs with various levels of formality and sophistication
+    """
+    This function generates a mad lib story based on user-selected levels of formality and sophistication for nouns, verbs, adjectives, and adverbs.
+    """
+
+    # Define lists of nouns, verbs, adjectives, and adverbs with varying levels of formality and sophistication
     nouns = {
         "common": ["dog", "cat", "elephant", "giraffe", "zebra"],
         "formal": ["gentleman", "lady", "scholar", "artist", "entrepreneur"],
@@ -31,6 +35,23 @@ def madlib_generator():
     verb_level = input("Enter verb level (common, action, poetic): ")
     adjective_level = input("Enter adjective level (common, descriptive, vibrant): ")
     adverb_level = input("Enter adverb level (common, manner, elegantly): ")
+
+    # Validate user input to ensure valid levels are chosen
+    if noun_level not in nouns:
+        print("Invalid noun level. Please choose from 'common', 'formal', or 'whimsical'")
+        return
+
+    if verb_level not in verbs:
+        print("Invalid verb level. Please choose from 'common', 'action', or 'poetic'")
+        return
+
+    if adjective_level not in adjectives:
+        print("Invalid adjective level. Please choose from 'common', 'descriptive', or 'vibrant'")
+        return
+
+    if adverb_level not in adverbs:
+        print("Invalid adverb level. Please choose from 'common', 'manner', or 'elegantly'")
+        return
 
     # Randomly select a word from each list based on the chosen levels
     noun = random.choice(nouns[noun_level])
